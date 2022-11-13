f3ab1ade1bd9c37c4cd4cf55a517fa52f033f7d7
doresu <9@lrdo.net>
2022-11-14 00:19:32 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,19 +45,19 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: '',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      name: 'qq',
+      link: 'https://qm.qq.com/cgi-bin/qm/qr?k=Mj_2_3r8S_MF7JqdriqwE8IcxXYl_rWu&jump_from=webapi&authKey=KPrDBmbv7hgrUoYbIpxMAR6rFww+iKALly+UXQ/61XGFpVPA2AyorOd1tC9cioIL',
     },
     {
       name: 'Telegram',
-      link: 'https://t.me/realSpencerWoo',
+      link: 'https://t.me/ssjjdgkd',
     },
   ],
 
