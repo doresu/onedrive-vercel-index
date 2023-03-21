98d2b14631ad6dbc172123e0baa73197e5b8d073
doresu <9@lrdo.net>
2023-03-21 23:21:29 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,8 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5843859428103999"
-     crossorigin="anonymous"></script>',
+    'onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
